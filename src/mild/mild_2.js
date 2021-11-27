719036bcfc4137644dee7f67fc4dcff7c2c08809
Maya Agnihotri <mayaagni@live.unc.edu>
2021-11-26 22:07:17 -0500

    new

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -55,7 +55,7 @@ export function identifyArray(array) {
  obj now does not contain the `password` field
  */
 export function removeKey(object, key) {
-   delete object.key;
+   delete object[key];
    return;
 }
 
@@ -76,10 +76,8 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-   let newObj = new Object();
-   newObj = object;
-   delete newObj.key;
-   return newObj;
+   const { key, ...objRest } = object;
+   return objRest;
 }
 
 /**
