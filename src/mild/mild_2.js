8cdf5741f737211879b5ff99d261e644ba658a23
Maya Agnihotri <mayaagni@live.unc.edu>
2021-11-26 22:13:53 -0500

    maya

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -76,9 +76,7 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-   let temp = key;
-   let { temp, ...objRest } = object;
-   return objRest;
+  
 }
 
 /**
