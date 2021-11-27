814796c677a93fbe7287bb2c66169d30b08b58e1
Maya Agnihotri <mayaagni@live.unc.edu>
2021-11-27 15:33:10 -0500

    i hate life

--- src/medium/medium_2.js
+++ src/medium/medium_2.js
@@ -7,7 +7,6 @@ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
 see under the methods section
 */
 
-
 /**
  * This object contains data that has to do with every car in the `mpg_data` object.
  *
@@ -19,10 +18,37 @@ see under the methods section
  *
  * @param {allCarStats.ratioHybrids} ratio of cars that are hybrids
  */
+
+let obj1 = new Object();
+let sum = 0;
+mpg_data.forEach(function(element) {
+    sum = sum + element.city_mpg;
+  })
+obj1['city'] = sum/mpg_data.length;
+sum = 0;
+mpg_data.forEach(function(element) {
+    sum = sum + element.highway_mpg;
+  })
+obj1['highway'] = sum/mpg_data.length;
+
+let obj2 = new Object();
+let arr2 = mpg_data.map(({ year }) => year);
+let temp2 = getStatistics(arr2);
+obj2['allCarStats'] = temp2;
+
+let obj3 = new Object();
+let sum3 = 0;
+mpg_data.forEach(function(element) {
+    if(element.hybrid) {
+        sum3++;
+    }
+  })
+obj3['rationHybrids'] = sum3/mpg_data.length;
+ 
 export const allCarStats = {
-    avgMpg: undefined,
-    allYearStats: undefined,
-    ratioHybrids: undefined,
+    avgMpg: obj1,
+    allYearStats: obj2,
+    ratioHybrids: obj3,
 };
 
 
