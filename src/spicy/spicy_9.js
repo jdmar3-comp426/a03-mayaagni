5c0349b34113a1bf9e633394c3738e0f1f08508d
Maya Agnihotri <mayaagni@live.unc.edu>
2021-11-27 20:02:15 -0500

    w

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -28,9 +28,9 @@ export const repeat = (fn, n, ...params) => {
  *   10 times.
  */
 export const repeatDemo = () => {
-    let a = `Hello, World!`
+    let a = `%0AHello, World!`
     let A = a.repeat(10);
-    console.log(A + ` `);
+    console.log(A);
 };
 
 
@@ -135,7 +135,7 @@ export const someEven = (arr, test) => {
         }
     }
     return false;
-    return;
+
 };
 
 
