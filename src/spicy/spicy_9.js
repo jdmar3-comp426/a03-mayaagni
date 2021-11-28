a45eef78ad94ee2269a7bb9a0eeffa4ab451aa2c
Maya Agnihotri <mayaagni@live.unc.edu>
2021-11-27 20:16:48 -0500

    f:

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -28,7 +28,7 @@ export const repeat = (fn, n, ...params) => {
  *   10 times.
  */
 export const repeatDemo = () => {
-    let a = `%0AHello, World!`
+    let a = ` Hello, World!`
     let A = a.repeat(10);
     console.log(A);
 };
@@ -187,10 +187,10 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-    if(everyEven(arr)) {
-        return false;
-    } else {
+    if(everyEven(arr, x => x%2 === 1)) {
         return true;
+    } else {
+        return false;
     }
 };
 
@@ -201,7 +201,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-    if(!everyEven(arr) || someEven(arr)) {
+    if(someEven(arr, x => x%2 === 1)) {
         return true;
     } else{
         return false;
